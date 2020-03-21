454dd97e099404a09fa8a73848ccaf5e6d15c84b
Erik Parawell <ac130kire@gmail.com>
2020-03-21 14:59:45 -0700

    added files

---
+++ server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>        
+#include <sys/ioctl.h>        
+#include <bits/ioctls.h>      
+#include <net/if.h>           
+#include <linux/if_ether.h>   
+#include <linux/if_packet.h>  
+#include <net/ethernet.h>
+
+#include <errno.h>
