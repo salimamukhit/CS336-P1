4d8c72ced625468a69297cb61b21c2cf3cfdce82
salimamukhit <smukhit@dons.usfca.edu>
2020-03-21 14:37:01 -0700

    networking proj

---
+++ network.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>        
+#include <sys/ioctl.h>        
+#include <bits/ioctls.h>      
+#include <net/if.h>           
+#include <linux/if_ether.h>   
+#include <linux/if_packet.h>  
+#include <net/ethernet.h>
+
+#include <errno.h>
+
+// Configuration file fills our structs
+
+// UDP connection being established with the server
+
+// 12000 packet trains are bing filled with high entropy and low entropy data
+
+// Send packet trains
+
+// We receive the ICMP packets that contain the arrival times
+
+// We decide whether compression took place or no
